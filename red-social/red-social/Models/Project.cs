92a7f096d334a669381d7fbfb764f97087748e7b
Franco castañeda <castanedafranco994@gmail.com>
2024-02-21 20:57:03 -0300

    modelos base

---
+++ red-social/red-social/Models/Project.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace red_social.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+    }
+}
---
+++ red-social/red-social/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace red_social.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public int NameUser { get; set; }
+        public int Password { get; set; }
+        public int Email { get; set; }
+        public List<Project> Projects { get; set; }
+
+    }
+}
